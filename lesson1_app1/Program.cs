b9dc11570c0b6ffbcc597eb847c36cc7b040d9ef
Semyon Ivanov <inary.wrk@gmail.com>
2021-01-18 23:43:54 +0300

    console app (lesson 1) show data and name

---
+++ lesson1_app1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lesson1_app1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            DateTime dateToday = DateTime.Today;
+            userName = Console.ReadLine();
+
+            while (String.IsNullOrEmpty(userName))
+            {
+                Console.WriteLine("epmty name");
+                userName = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"Hello, {userName}, today {dateToday.ToString("d")}");
+
+            Console.ReadLine();
+        }
+    }
+}
