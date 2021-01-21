e5c1900a52ac2ff159d32721ba403cf2ac43f5a9
Semyon <inary.wrk@gmail.com>
2021-01-21 12:25:13 +0300

    Parity check two methods

--- lesson2/App3Even/Program.cs
+++ lesson2/App3Even/Program.cs
@@ -6,15 +6,38 @@ namespace App3Even
     {
         static void Main(string[] args)
         {
-            int knowledges = 0b011111;
-            int back = 0b010111;
-            int front = 0b011010;
-            int designer = 0b110000;
+            Console.Write("Parity check, please enter the number: ");
+            var number = Console.ReadLine();
+            int intNumber;
 
-            Console.WriteLine((knowledges & back) == back);
-            Console.WriteLine((knowledges & front) == front);
-            Console.WriteLine((knowledges & designer) == designer);
-            
+            while (Int32.TryParse(number, out intNumber) == false)
+            {
+                Console.Write("The number must be an integer, please re-enter: ");
+                number = Console.ReadLine();
+            }
+
+            if (intNumber % 2 == 1)
+
+            {
+                Console.WriteLine("The number {0} is not even. Method - remainder of division by 2.", number);
+
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is even. Method - remainder of division by 2.", number);
+            }
+
+            if ((intNumber & 1) == 1)
+
+            {
+                Console.WriteLine("The number {0} is not even. Method - bitwise multiplication.", number);
+
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is even. Method - bitwise multiplication.", number);
+            }
+
             Console.ReadLine();
         }
     }
