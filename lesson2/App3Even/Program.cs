a2477c3bd7a6dd77dbee51a2418abcfea78bc904
Semyon <inary.wrk@gmail.com>
2021-01-21 10:29:22 +0300

    add App3Even

---
+++ lesson2/App3Even/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace App3Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int knowledges = 0b011111;
+            int back = 0b010111;
+            int front = 0b011010;
+            int designer = 0b110000;
+
+            Console.WriteLine((knowledges & back) == back);
+            Console.WriteLine((knowledges & front) == front);
+            Console.WriteLine((knowledges & designer) == designer);
+            
+            Console.ReadLine();
+        }
+    }
+}
