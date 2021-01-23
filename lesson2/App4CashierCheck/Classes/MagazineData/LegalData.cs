9717d728df5c92f85a79a0b6642d86fbe2ef01a5
Semyon <inary.wrk@gmail.com>
2021-01-23 06:55:54 +0300

    fixed bugs, logic change

--- lesson2/App4CashierCheck/Classes/MagazineData/LegalData.cs
+++ lesson2/App4CashierCheck/Classes/MagazineData/LegalData.cs
@@ -5,8 +5,8 @@ namespace App4CashierCheck.Classes
 
     public class LegalData
     {
-        public uint kKM;
-        public uint iNN;
+        public ulong kKM;
+        public ulong iNN;
         public uint numEKLZ;
         public string idEKLZ;
 
@@ -18,12 +18,12 @@ namespace App4CashierCheck.Classes
             idEKLZ = SetIdEKLZ();
         }
 
-        public uint SetKKM()
+        public ulong SetKKM()
         {
             Console.Write("Enter the KKM: ");
             string _kKM = Console.ReadLine();
-            uint kKM;
-            while ((UInt32.TryParse(_kKM, out kKM) == false) || (kKM < 0))
+            ulong kKM;
+            while ((ulong.TryParse(_kKM, out kKM) == false) || (kKM < 0))
             {
                 Console.Write("The KKM must be a positive integer, please re - enter: ");
                 _kKM = Console.ReadLine();
@@ -31,12 +31,12 @@ namespace App4CashierCheck.Classes
             return kKM;
         }
 
-        public uint SetINN()
+        public ulong SetINN()
         {
             Console.Write("Enter the INN: ");
             string _iNN = Console.ReadLine();
-            uint iNN;
-            while ((UInt32.TryParse(_iNN, out iNN) == false) || (iNN < 0) || (_iNN.Length != 12))
+            ulong iNN;
+            while ((ulong.TryParse(_iNN, out iNN) == false) || (iNN < 0) || (_iNN.Length != 12))
             {
                 Console.Write("The INN must be a positive integer and consist of 12 digits, please re - enter: ");
                 _iNN = Console.ReadLine();
@@ -46,13 +46,13 @@ namespace App4CashierCheck.Classes
 
         public uint SetNumEKLZ()
         {
-            Console.Write("Enter number of EKLZ: ");
+            Console.Write("Enter the EKLZ number: ");
             string numEKLZ = Console.ReadLine();
             uint number;
 
             while ((UInt32.TryParse(numEKLZ, out number) == false) || (number < 0))
             {
-                Console.Write("The number of EKLZ must be a positive integer, please re - enter: ");
+                Console.Write("EKLZ number must be a positive integer, please re - enter: ");
                 numEKLZ = Console.ReadLine();
             }
             return number;
@@ -60,9 +60,8 @@ namespace App4CashierCheck.Classes
 
         public string SetIdEKLZ()
         {
-            Console.Write("Enter id of EKLZ: ");
-            string idEKLZ = Console.ReadLine();
-            return idEKLZ;
+            Console.Write("Enter EKLZ id: ");
+            return Console.ReadLine();
         }
     }
 }
--- lesson2/App4CashierCheck/Program.cs
+++ lesson2/App4CashierCheck/Program.cs
@@ -8,10 +8,15 @@ namespace App4CashierCheck
     {
         static void Main(string[] args)
         {
-            var a =@Console.ReadLine();
-            @Console.WriteLine(a);
-         
-            
+            //var newMagazine = new Shop();
+            //var newCheck = new Receipt();
+            //Console.WriteLine($"Magazine: {newMagazine.shop}, Cashier: {newMagazine.cashier}, INN: {newMagazine.data.iNN}, KKM: {newMagazine.data.kKM}\n" +
+            //    $"Number EKLZ: {newMagazine.data.numEKLZ}, Id EKLZ: {newMagazine.data.idEKLZ}");
+            //Console.WriteLine($"Check: {newCheck.number}, : {newMagazine.cashier}, INN: {newMagazine.data.iNN}, KKM: {newMagazine.data.kKM}\n" +
+            //    $"Number EKLZ: {newMagazine.data.numEKLZ}, Id EKLZ: {newMagazine.data.idEKLZ}");
+            int i = 1;
+            i += (2 * 5);
+            Console.WriteLine(i);
             Console.ReadLine();
         }
     }
