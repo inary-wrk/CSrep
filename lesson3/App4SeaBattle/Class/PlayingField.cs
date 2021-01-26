b9d0aeb74d81bb3d34bf45e72d2b639a6ecf42c2
Semyon <inary.wrk@gmail.com>
2021-01-26 19:12:18 +0300

    class player field

---
+++ lesson3/App4SeaBattle/Class/PlayingField.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace App4SeaBattle.Class
+{
+    class PlayingField
+    {
+        public char[,] player1FieldShips = new char[12, 12];
+        public char[,] player1FieldShots = new char[12, 12];
+        public char[,] player2FieldShips = new char[12, 12];
+        public char[,] player2FieldShots = new char[12, 12];
+
+        public PlayingField()
+        {
+            for (int i = 2; i <=11; i++)
+            {
+                for (int j = 2; j < 11; j++)
+                {
+                    player1FieldShips[i, j] = '~';
+                    player1FieldShots[i, j] = '~';
+                    player2FieldShips[i, j] = '~';
+                    player2FieldShots[i, j] = '~';
+                }
+            }
+        }
+        public void GetPlayingField(char[,] field)
+        {
+            for (int i = 0; i <= 11; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write(field[i,j]);
+                }
+            }
+        }
+    }
+}
---
+++ lesson3/App4SeaBattle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using App4SeaBattle.Class;
+namespace App4SeaBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayingField field = new PlayingField();
+            field.GetPlayingField(field.player1FieldShips);
+            Console.ReadLine();
+        }
+    }
+}
