b4e0279486f5a2379895ea4df29d3f2a100260ed
Semyon <inary.wrk@gmail.com>
2021-01-28 19:37:39 +0300

    create game field

--- lesson3/App4SeaBattle/Class/PlayingField.cs
+++ lesson3/App4SeaBattle/Class/PlayingField.cs
@@ -4,34 +4,65 @@ namespace App4SeaBattle.Class
 {
     class PlayingField
     {
-        public char[,] player1FieldShips = new char[12, 12];
-        public char[,] player1FieldShots = new char[12, 12];
-        public char[,] player2FieldShips = new char[12, 12];
-        public char[,] player2FieldShots = new char[12, 12];
+        public static int x = 11, y = 11;
+        public static string water = "≈";
+        public string[,] player1FieldShips = new string[x, y];
+        public string[,] player1FieldShots = new string[x, y];
+        public string[,] player2FieldShips = new string[x, y];
+        public string[,] player2FieldShots = new string[x, y];
 
         public PlayingField()
         {
-            for (int i = 2; i <=11; i++)
+            FillField(player1FieldShips);
+            FillField(player2FieldShips);
+            FillField(player1FieldShots);
+            FillField(player2FieldShots);
+        }
+
+
+        public void FillField(string[,] field)
+        {
+            char a = 'A';
+            for (int i = 1; i < x; i++)
             {
-                for (int j = 2; j < 11; j++)
+                for (int j = 1; j < y; j++)
                 {
-                    player1FieldShips[i, j] = '~';
-                    player1FieldShots[i, j] = '~';
-                    player2FieldShips[i, j] = '~';
-                    player2FieldShots[i, j] = '~';
+                    field[i, j] = water;
+                    field[i, 0] = i.ToString();
                 }
+                field[0, i] = a.ToString() + "|";
+                a++;
             }
         }
-        public void GetPlayingField(char[,] field)
+
+        public void GetPlayingField(string[,] fieldShips, string[,] fieldShots)
         {
-            for (int i = 0; i <= 11; i++)
+            Console.ForegroundColor = ConsoleColor.Blue;
+            for (int i = 3; i < x * 3; i += 3)
             {
-                for (int j = 0; j < 11; j++)
+                for (int j = 1; j < y; j++)
                 {
                     Console.SetCursorPosition(i, j);
-                    Console.Write(field[i,j]);
+                    Console.Write(fieldShips[i / 3, j]);
+                    Console.SetCursorPosition(i + 40, j);
+                    Console.Write(fieldShots[i / 3, j]);
                 }
             }
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 1; i < x * 3; i += 3)
+            {
+                var j = i / 3;
+                Console.SetCursorPosition(i - 1, 0);
+                Console.Write(fieldShips[j, 0]);
+                Console.SetCursorPosition(i + 39, 0);
+                Console.Write(fieldShots[j, 0]);
+
+                Console.SetCursorPosition(0, j);
+                Console.Write(fieldShips[0, j]);
+                Console.SetCursorPosition(0 + 40, j);
+                Console.Write(fieldShots[0, j]);
+            }
+
         }
     }
 }
--- lesson3/App4SeaBattle/Program.cs
+++ lesson3/App4SeaBattle/Program.cs
@@ -6,8 +6,45 @@ namespace App4SeaBattle
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
             PlayingField field = new PlayingField();
-            field.GetPlayingField(field.player1FieldShips);
+            ////field.player1FieldShips[2, 2] = '^';
+            ////field.player1FieldShips[1, 2] = '[';
+            ////field.player1FieldShips[3, 2] = ']';
+            ////field.player1FieldShips[1, 3] = '[';
+            ////field.player1FieldShips[2, 3] = 'o';
+            ////field.player1FieldShips[3, 3] = ']';
+            ////field.player1FieldShips[1, 4] = '[';
+            ////field.player1FieldShips[2, 4] = '_';
+            ////field.player1FieldShips[3, 4] = ']';
+            //;
+            //field.player1FieldShips[1, 2] = '│';
+            //field.player1FieldShips[2, 2] = '^';
+            //field.player1FieldShips[3, 2] = '│';
+            //field.player1FieldShips[1, 3] = '│';
+            //field.player1FieldShips[2, 3] = 'o';
+            //field.player1FieldShips[3, 3] = '│';
+            //field.player1FieldShips[1, 4] = '│';
+            //field.player1FieldShips[2, 4] = '▄';
+            //field.player1FieldShips[3, 4] = '│';
+
+            //field.player1FieldShips[8, 4] = '_';
+            //field.player1FieldShips[8, 5] = '>';
+            //field.player1FieldShips[8, 6] = '̅';
+            //field.player1FieldShips[7, 6] = '̅';
+            //field.player1FieldShips[7, 6] = '̅';
+            //field.player1FieldShips[7, 6] = '̅';
+            //field.player1FieldShips[6, 6] = '̅';
+            //field.player1FieldShips[6, 6] = '̅';
+            //field.player1FieldShips[6, 6] = '̅';
+            //field.player1FieldShips[5, 4] = '_';
+            //field.player1FieldShips[5, 5] = 'o';
+            //field.player1FieldShips[5, 6] = '̅';
+            //field.player1FieldShips[4, 4] = '_';
+            //field.player1FieldShips[4, 5] = '▌';
+            //field.player1FieldShips[4, 6] = '̅';
+
+            field.GetPlayingField(field.player1FieldShips, field.player1FieldShots);
             Console.ReadLine();
         }
     }
