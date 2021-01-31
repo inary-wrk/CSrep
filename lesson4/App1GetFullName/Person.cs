f354d77afc0f2f5acd914f110f254f6646f75d75
Semyon <inary.wrk@gmail.com>
2021-01-31 20:23:23 +0300

    app get full name

---
+++ lesson4/App1GetFullName/Person.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace App1GetFullName
+{
+    public class Person
+    {
+        private string name;
+        private string lastName;
+        private string patronymic;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value)) name = "";
+                else name = value;
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value)) lastName = "";
+                else lastName = value;
+            }
+        }
+        public string Patronymic
+        {
+            get => patronymic;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value)) patronymic = "";
+                else patronymic = value;
+            }
+        }
+
+
+        public Person(string name, string lastName, string patronymic)
+        {
+            Name = name;
+            LastName = lastName;
+            Patronymic = patronymic;
+        }
+
+
+        public Person()
+        {
+            Name = EnterName("Name");
+            LastName = EnterName("Last Name");
+            Patronymic = EnterName("Patronymic");
+        }
+
+
+        public string EnterName(string whatName)
+        {
+            bool logic = true;
+            while (logic)
+            {
+                Console.Write("Enter the {0}: ", whatName);
+                name = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(name))
+                {
+                    Console.Write("If you want to leave the {0} blank, type Y", whatName);
+                    Console.ReadKey();
+                   logic = Console.ReadKey() ==  ? false : true;
+                }
+            }
+            return name;
+        }
+
+
+        public string FullName()
+        {
+            return $"{Name} {LastName} {Patronymic}";
+        }
+
+    }
+}
---
+++ lesson4/App1GetFullName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace App1GetFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Person person1 = new Person();
+        Person person2 = new Person("Alexander", "Ivanov", "Temurovich");
+        Person person3 = new Person("Philip", "Elens", "");
+
+            Console.WriteLine(person1.FullName());
+            Console.WriteLine(person2.FullName());
+            Console.WriteLine(person3.FullName());
+        }
+
+    }
+}
