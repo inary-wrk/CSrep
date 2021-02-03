e13719b062d589d54e275eb2c4cac39b797cb05a
Semyon <inary.wrk@gmail.com>
2021-02-03 19:46:06 +0300

    startup time format updated

--- lesson5/App2TimeStartup/Program.cs
+++ lesson5/App2TimeStartup/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿// Написать программу, которая при старте дописывает текущее время в файл «startup.txt».
+
+
+using System;
+using System.IO;
+
 
 namespace App2TimeStartup
 {
-    class Program
+    class StartUpTimeDate
     {
+        private static readonly DateTime startUpTime = DateTime.UtcNow;
+
+
+        /// <value>
+        /// Get StartUp time/date UTC format.
+        /// </value>
+        public static DateTime StartUpTime { get => startUpTime; }
+
+
+        /// <summary>
+        /// Append <paramref name="time"/> to <paramref name="file"/> in UTC and local format.
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="file"></param>
+        public static void TimeToFile(DateTime time, string file)
+        {
+            File.AppendAllText(file, $"{time.ToUniversalTime()} (UTC) \n{time.ToLocalTime()} Local time {TimeZoneInfo.Local}\n\n");
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.ReadLine();
+            TimeToFile(StartUpTime, "startup.txt");
         }
     }
 }
