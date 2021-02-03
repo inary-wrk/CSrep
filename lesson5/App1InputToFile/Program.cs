209657e5911503012db3cfe66de0902e92fa850b
Semyon <inary.wrk@gmail.com>
2021-02-03 03:27:15 +0300

    new

---
+++ lesson5/App1InputToFile/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+
+using System;
+using System.IO;
+
+
+namespace App1InputToFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("text.txt", Console.ReadLine());
+
+        }
+    }
+}
