5ef76bfae7c454c6ab288af4b46a4c111984026e
Semyon <inary.wrk@gmail.com>
2021-02-05 02:48:02 +0300

    add CorrectPath Class

---
+++ lesson5/App3NumberToBiteFile/Program.cs
@@ -0,0 +1,47 @@
+﻿// 3. Ввести с клавиатуры произвольный набор чисел (0...255) и записать их в бинарный файл.
+
+using System;
+using System.IO;
+
+
+
+namespace App3NumberToBiteFile
+{
+    class Program
+    {
+        static void EnterPath(byte[] numbers)
+        {
+            //while (true)
+            //{
+            //    Console.WriteLine("Enter the path to the file, if the file does not exist it will be created:");
+            //   // Directory.Exists()
+            //}
+            try
+            {
+                File.WriteAllBytes(Console.ReadLine(), numbers);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid path, The file bytes.bin was generated in the executable file folder.");
+                File.WriteAllBytes("bytes.bin", numbers);
+            }
+
+        }
+
+        static byte EnterNumber()
+        {
+            do
+            {
+                Console.Write("Enter an integer nuber in the range 0..255: ");
+                if (Byte.TryParse(Console.ReadLine(), out byte number)) return number;
+            } while (true);
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(EnterNumber());
+            Console.WriteLine(System.IO.Path.GetInvalidPathChars());
+        }
+    }
+}
---
+++ lesson5/CorrectPath.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using 
+
+public class CorrectPath
+{
+	public bool CorrectPathCheck(string path)
+    {
+        
+        try
+        {
+            if (File.Exists(path)) return true;
+            File.Create(path);
+            return true;
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine("No access to the file.");
+            return false;
+        }
+        catch (ArgumentException)
+        catch (ArgumentNullException)
+        catch (PathTooLongException)
+        catch (NotSupportedException)
+
+        {
+            Console.WriteLine("Invalid path.");
+            return false;
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("An I/O error occurred while creating the file.");
+            return false;
+        }
+        catch (DirectoryNotFoundException)
+        {
+            if (path.Contains(@"\"))
+            {
+                Directory.CreateDirectory(path);
+            }
+            else
+            {
+                return false;
+            }
+        }    
+        catch (Exception)
+        {
+
+            throw;
+        }
+
+    }
+}
