9db184e8e6db18cb64e76418c15e33091cc5c34a
Semyon <inary.wrk@gmail.com>
2021-02-05 05:04:13 +0300

    update

--- lesson5/CorrectPath.cs
+++ lesson5/CorrectPath.cs
@@ -4,50 +4,80 @@ using
 
 public class CorrectPath
 {
-	public bool CorrectPathCheck(string path)
+
+
+    public bool CorrectDirectoryPath(in string path out Exception ex)
     {
-        
+        bool logic = false;
+        try
+        {
+            Directory.CreateDirectory
+            logic = true;
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine("No access to the file.");
+        }
+        catch (ArgumentException ex)
+        catch (ArgumentNullException ex)
+        catch (PathTooLongException ex)
+        catch (NotSupportedException ex)
+        {
+            Console.WriteLine("Invalid path.");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("An I/O error occurred while creating the file.");
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine("Directory not found Exception");
+        }
+        catch
+        {
+            Console.WriteLine("Unknown error.");
+        }
+
+        return logic;
+    }
+
+
+	public bool CorrectFilePath(in string path out string ex)
+    {
+        bool logic = false;
         try
         {
             if (File.Exists(path)) return true;
             File.Create(path);
-            return true;
+            File.Delete(path);
+            logic = true;
         }
-        catch (UnauthorizedAccessException)
+        catch (UnauthorizedAccessException ex)
         {
             Console.WriteLine("No access to the file.");
-            return false;
         }
-        catch (ArgumentException)
-        catch (ArgumentNullException)
-        catch (PathTooLongException)
-        catch (NotSupportedException)
-
+        catch (ArgumentException ex)
+        catch (ArgumentNullException ex)
+        catch (PathTooLongException ex)
+        catch (NotSupportedException ex)
         {
             Console.WriteLine("Invalid path.");
-            return false;
         }
-        catch (IOException)
+        catch (IOException ex)
         {
             Console.WriteLine("An I/O error occurred while creating the file.");
-            return false;
         }
-        catch (DirectoryNotFoundException)
+        catch (DirectoryNotFoundException ex)
         {
-            if (path.Contains(@"\"))
-            {
-                Directory.CreateDirectory(path);
-            }
-            else
-            {
-                return false;
-            }
+            Console.WriteLine("Directory not found Exception");
         }    
-        catch (Exception)
+        catch
         {
-
-            throw;
+            Console.WriteLine("Unknown error.");
         }
+        
+        return logic;
+        
 
     }
 }
---
+++ lesson5/test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace test
+{
+    class Program
+    {
+        static bool test(int x, int y)
+        {
+            bool logic = false;
+            try
+            {
+                if (x == 0)
+                {
+
+                Console.WriteLine(x/y);  return true;
+                }
+
+                Console.WriteLine(2);
+            }
+            catch (Exception)
+            {
+                logic = true;
+            }
+            
+            
+
+            
+            
+                return logic;
+        }
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine(test(0,0));
+            Console.WriteLine(Directory.Exists(@"new\second\file.txt")); 
+        }
+    }
+}
