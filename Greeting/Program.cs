d32d85dce39b3414ca1f13c15a90593c0789cb91
Semyon <inary.wrk@gmail.com>
2021-02-16 19:53:40 +0300

    ready greeting app

---
+++ Greeting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conf = Properties.Settings.Default;
+            Console.WriteLine(conf.greeting);
+            if (conf.name != String.Empty || conf.age != 0 || conf.occupation != String.Empty)
+            {
+                Console.WriteLine($"Name: {conf.name}\n" +
+                                  $"Age: {conf.age}\n" +
+                                  $"Occupation: {conf.occupation}\n");
+            }
+
+
+            Console.Write("Enter\nName: ");
+            conf.name = Console.ReadLine();
+            Console.Write("Age: ");
+            try
+            {
+                conf.age = Byte.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                conf.age = 0;
+            }
+            Console.Write("Occupation: ");
+            conf.occupation = Console.ReadLine();
+            conf.Save();
+
+        }
+    }
+}
