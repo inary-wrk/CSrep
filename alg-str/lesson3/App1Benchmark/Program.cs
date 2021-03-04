fe6fa918170610e47ad772aefc959a942ad59c3f
Semyon <inary.wrk@gmail.com>
2021-03-04 19:48:30 +0300

    add empty solution

---
+++ alg-str/lesson3/App1Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App1Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
