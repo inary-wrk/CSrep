7148b9b78e36793c9d9d701f61c5ae02842532f1
Semyon <inary.wrk@gmail.com>
2021-03-09 08:52:39 +0300

    Creat branch

---
+++ alg-str/lesson4/App1Search/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App1Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
