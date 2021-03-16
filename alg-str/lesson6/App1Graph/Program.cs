02cf6b44490dfa4fe9878d4e588b8ce9f7b5b1d4
Semyon <inary.wrk@gmail.com>
2021-03-16 19:41:03 +0300

    empty project

---
+++ alg-str/lesson6/App1Graph/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App1Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
