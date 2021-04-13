ed4003483f4462543b8a45b3939748c2932c57bc
Semyon <inary.wrk@gmail.com>
2021-04-13 09:31:19 +0300

    wip

--- alg-str/lesson6/App1Graph/Program.cs
+++ alg-str/lesson6/App1Graph/Program.cs
@@ -1,11 +1,27 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace App1Graph
 {
+    class test
+    {
+      public  static int[] x = new int[10];
+    }
     class Program
     {
+        static void TEstM<T>(ICollection<T> col)
+        {
+            var en = col.GetEnumerator();
+            while (en.MoveNext())
+            {
+                Console.WriteLine(en.Current);
+            }
+        }
+
         static void Main(string[] args)
         {
+            var en = test.x.GetEnumerator();
+            en.MoveNext();
+            TEstM(test.x);
             Console.WriteLine("Hello World!");
         }
     }
